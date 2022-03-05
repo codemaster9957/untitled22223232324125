df97b9f49b9a7c0bece07319f00f3b48ef043e19
codemaster9957 <98223070+codemaster9957@users.noreply.github.com>
2022-03-05 09:43:15 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -478,6 +478,32 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSp
         info.startCountdown(1)
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.later, function (sprite, otherSprite) {
+    if (controller.A.isPressed()) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . c c c a c . . . . 
+            . . c c b b b a c a a a c . . . 
+            . c c a b a c b a a a b c c . . 
+            . c a b c f f f b a b b b a . . 
+            . c a c f f f 8 a b b b b b a . 
+            . c a 8 f f 8 c a b b b b b a . 
+            c c c a c c c c a b c f a b c c 
+            c c a a a c c c a c f f c b b a 
+            c c a b 6 a c c a f f c c b b a 
+            c a b c 8 6 c c a a a b b c b c 
+            c a c f f a c c a f a c c c b . 
+            c a 8 f c c b a f f c b c c c . 
+            . c b c c c c b f c a b b a c . 
+            . . a b b b b b b b b b b b c . 
+            . . . c c c c b b b b b c c . . 
+            . . . . . . . . c b b c . . . . 
+            `, mySprite, 100, 0)
+        controller.combos.setTriggerType(TriggerType.Continuous)
+    }
+})
+controller.combos.attachSpecialCode(function () {
+	
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (info.score() == 2) {
         statusbar2.value += -10
